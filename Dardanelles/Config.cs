5235547cfade7b7b94d8ca66ca2f7c4492d1a92e
Weston Sleeman <westonsleeman@hotmail.com>
2018-12-21 11:18:23 -0800

    Initial commit.

---
+++ Dardanelles/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Dardanelles
+{
+    class Config
+    {
+        private readonly FileInfo Config;
+        public Config(string filename)
+        {
+            if (File.Exists(filename))
+                path = (new FileInfo(filename)).
+        }
+    }
+}
